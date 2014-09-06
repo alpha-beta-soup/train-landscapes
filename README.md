c0cc2dfd1a51bdc254f3fe9dc91320a8b4805348
alpha-beta-soup <richard.m.law@gmail.com>
2014-09-07 11:49:19 +1200

    GRASS 6.4 version of the original; prepared it for roads rather than rail.

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 train-landscapes
 ================
 
-Viewshed analysis of a train route
+Viewshed analysis of a line feature draped over a DEM
+
+Requirements
+------------
+
+Assumes [GRASS 6.4](http://grass.osgeo.org/); see original repo (by [@pierreroudier](https://github.com/pierreroudier)) for a GRASS 7.0 equivalent.
--- code/generate_los.sh
+++ code/generate_los.sh
@@ -1,14 +1,18 @@
 #/bin/bash
+# Grass 6.4
 
-PTS_FILE="./rail_points_coords.csv"
-DIST_PTS=100
-MAX_VIS_DIST=30000
+# Load shapefile into GRASS
+v.in.ogr dsn=roads.shp output=road -o
+
+PTS_FILE="./road_points_coords.csv"
+DIST_PTS=25 # Ideally the resolution of the DEM
+MAX_VIS_DIST=30000 # Maximum distance visible
 
 # Sample points along line
-v.to.points in=rail out=rail_points dmax=$DIST_PTS --o --q
+v.to.points -nvit in=roadsg out=roads_points dmax=$DIST_PTS --o --q
 
 # Put point coordinates in text file
-v.out.ascii --q in=rail_points sep=, | awk -F "\"*,\"*" '{print $1","$2}' > $PTS_FILE
+v.out.ascii -r in=roads_points fs=, --quiet | awk -F "\"*,\"*" '{print $1","$2}' > $PTS_FILE
 
 NPTS=`cat $PTS_FILE | wc -l`
 
@@ -26,7 +30,7 @@ while read -r line
 
   echo -ne "Processing $NPTS viewshed instances: \t $PCT % \r" 
   
-  r.viewshed -crb in=dem out=los_${COUNTER} coord=$line max_dist=$MAX_VIS_DIST memory=1500 --o --q
+  r.los -c input=dem output=los_${COUNTER} coordinate=$line max_dist=$MAX_VIS_DIST --o --q
   COUNTER=$((COUNTER+1))
   
 done < $PTS_FILE
@@ -38,10 +42,10 @@ echo "\n"
 #   this as a boolean mask)
 r.series in=`g.mlist --q type=rast pat=los_* sep=,` out=total_los method=sum --o
 
-# Create distance to railway map
-v.to.rast in=rail out=rail use=val val=1 --o
-r.grow.distance -m input=rail distance=dist_from_rail --o --q
+# Create distance to road map
+v.to.rast in=road out=road use=val val=1 --o
+r.grow.distance -m input=road distance=dist_from_road --o --q
 
-# Use distance to railway instead of viewing angle in the
+# Use distance to road instead of viewing angle in the
 # viewshed result map 
-r.mapcalc "dist_los = if(total_los, dist_from_rail, null())" --o --q
+r.mapcalc "dist_los = if(total_los, dist_from_road, null())" --o --q
