17209dbdeb67fc63ef78e02b3db362ac159b26f9
alpha-beta-soup <richard.m.law@gmail.com>
2014-09-08 09:31:25 +1200

    r.los manual recommends that the maximum visibibility distance does not exceed 1000 rows and columns (500 cells in any one direction from the observer). Added a constrain that MAX_VIS_DIST does not exceed this given the raster resolution (new param R_RES).

--- code/generate_los.sh
+++ code/generate_los.sh
@@ -3,12 +3,24 @@
 LINE_SHP='../data/roads_test.shp' # Roads shapefile
 R_DEM='../data/nidemreproj' # Elevation DEM
 PTS_FILE="../data/road_points_coords.csv" # An intermediate output of this script; coordinates of LINE_SHP
-DIST_PTS=25 # Ideally the resolution of the DEM
+R_RES=25 # The resolution of the DEM (metres)
+DIST_PTS=100000 # Ideally the resolution of the DEM
 MAX_VIS_DIST=30000 # Maximum distance visible
-ELEV=2 # Metres above the ground that the observer stands (note, try include the vehicle, too)
+ELEV=1.2 # Metres above the ground that the observer stands (note, try include the vehicle, too)
+
+# r.los takes a long time, and the manual says to keep the number of rows and columns
+#   "under 1000". Here we adjust MAX_VIS_DIST by considering the resolution of
+#   the raster (R_RES), so that there is a maximum of 1000 rows and columns
+POS_VIS_DIST=$(echo "$R_RES * 1000 / 2" | bc -l) # The maximum possible vis dist to use and still have 1000 rows and cols
+if [ 1 -eq `echo "$POS_VIS_DIST < $MAX_VIS_DIST" | bc` ]
+then
+  MAX_VIS_DIST=$POS_VIS_DIST
+else
+  MAX_VIS_DIST=$MAX_VIS_DIST
+fi
 
 # Load shapefile into GRASS
-v.in.ogr dsn=$LINE_SHP output=road -o --verbose
+v.in.ogr dsn=$LINE_SHP output=road --o --v
 
 # Load elevation raster into GRASS and set it as the computational region
 r.in.gdal --o input=$R_DEM output=dem --verbose
@@ -31,23 +43,24 @@ while read -r line
   do
   
   PCT_FLOAT=$(echo "100*$((COUNTER+1))/$NPTS" | bc -l)
-  PCT=`printf "%0.1f\n" $PCT_FLOAT` 
+  PCT=`printf "%0.1f\n" $PCT_FLOAT`
 
-  echo -ne "Processing $NPTS viewshed instances: \t $PCT % \r" 
+  echo -ne "Processing $NPTS viewshed instances: \t $PCT % ($COUNTER/$NPTS) \r" 
   
   # Set the region to a smaller subset around the current observer point
-  # to speed processing
+  #   to speed processing
   x=$(echo $line | cut -f1 -d,)
   y=$(echo $line | cut -f2 -d,)
-   
   W=$(echo "$x-$MAX_VIS_DIST" | bc -l)
   E=$(echo "$x+$MAX_VIS_DIST" | bc -l)
   N=$(echo "$y+$MAX_VIS_DIST" | bc -l)
   S=$(echo "$y-$MAX_VIS_DIST" | bc -l)
-  
   g.region n=$N s=$S e=$E w=$W
   
-  r.los input=dem output=tmp_los_${COUNTER} coordinate=$line obs_elev=$ELEV max_dist=$MAX_VIS_DIST --o
+  # Does not overwrite, so SIGINT (Ctrl+C) can be used to interrupt a
+  #  long-running process, to be resumed later
+  #  (keep parameters constant between runs)
+  r.los input=dem output=tmp_los_${COUNTER} coordinate=$line obs_elev=$ELEV max_dist=$MAX_VIS_DIST --v
   COUNTER=$((COUNTER+1))
   
 done < $PTS_FILE
@@ -67,8 +80,8 @@ v.to.rast in=road out=road use=val val=1 --o --q
 r.grow.distance -m input=road distance=dist_from_road --o --q
 
 # Use distance to road instead of viewing angle in the
-# viewshed result map 
+#   viewshed result map 
 r.mapcalc "dist_los = if(total_los, dist_from_road, null())"
 
 # Clean up, removing the component visibility rasters
-g.mremove -f "tmp_los_*" --v
+g.mremove -f "tmp_los_*" --q
