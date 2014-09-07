5ffedbf53abc7d895bd84f90a5bf7cef788c65b5
alpha-beta-soup <richard.m.law@gmail.com>
2014-09-07 17:26:27 +1200

    Squishy bugs in GRASS 6.4 parameters. Changes to README. Added variable for observer height.

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 train-landscapes
 ================
 
-Viewshed analysis of a line feature draped over a DEM
+Viewshed analysis of a line feature draped over a DEM.
 
 How to
 ------
 
 First, obtain a raster digital elevation model. Edit the variable `R_DEM` to point to it. Viewshed will be determined on this surface.
 
-Secondly, obtain a line vector feature (a shapefile's good). Edit the variable `LINE_SHP` tp point to it. You may like to simplify the line or edit out the tunnels (where nothing is generally visible).
+Secondly, obtain a line vector feature (a shapefile's good). Edit the variable `LINE_SHP` to point to it. You may like to simplify the line or edit out the tunnels (where nothing is generally visible). The viewshed will be determined using a sample of points along this line as observer locations.
 
 1. In a terminal: `$ grass64`
 2. (Set up your GRASS workspace)
@@ -21,3 +21,10 @@ Requirements
 ------------
 
 Assumes [GRASS 6.4](http://grass.osgeo.org/); see original repo (by [@pierreroudier](https://github.com/pierreroudier)) for a GRASS 7.0 equivalent.
+
+Inspired by
+-----------
+
+A long beautiful [train journey](http://www.kiwirailscenic.co.nz/northern-explorer/) in New Zealand, the economics of choosing a train over driving, and this [blog post](http://datagistips.blogspot.co.nz/2011/09/on-road-with-r-grass-intervisibility.html) on doing the same thing with R/GRASS.
+
+![Tranz Scenic's Northern Explorer, with the volcanic zone in the background](https://github.com/alpha-beta-soup/train-landscapes/raw/master/img.jpg)
--- code/generate_los.sh
+++ code/generate_los.sh
@@ -1,28 +1,30 @@
 #/bin/bash
 # Grass 6.4
 
-LINE_SHP='./roads.shp'
-R_DEM='./nidemreproj.tiff'
-PTS_FILE="./road_points_coords.csv"
-DIST_PTS=25 # Ideally the resolution of the DEM
-MAX_VIS_DIST=30000 # Maximum distance visible
+LINE_SHP='../data/roads_test.shp' # Roads shapefile
+R_DEM='../data/nidemreproj' # Elevation DEM
+PTS_FILE="../data/road_points_coords.csv" # An intermediate output of this script; coordinates of LINE_SHP
+DIST_PTS=10000 # Ideally the resolution of the DEM
+MAX_VIS_DIST=100 # Maximum distance visible
+ELEV=2 # Metres above the ground that the observer stands (note, try include the vehicle, too)
 
 # Load shapefile into GRASS
-v.in.ogr dsn=$LINE_SHP output=road -o
+v.in.ogr dsn=$LINE_SHP output=road -o --verbose
 
 # Load elevation raster into GRASS and set it as the computational region
-r.in.gdal --o input=$R_DEM output=dem --verbose
+#r.in.gdal --o input=$R_DEM output=dem --verbose
+g.region -pm rast=dem --verbose
 
 # Sample points along line
-v.to.points -nvit in=roadsg out=roads_points dmax=$DIST_PTS --o --q
+v.to.points in=road out=roads_points dmax=$DIST_PTS --o --q
 
 # Put point coordinates in text file
 v.out.ascii -r in=roads_points fs=, --quiet | awk -F "\"*,\"*" '{print $1","$2}' > $PTS_FILE
 
 NPTS=`cat $PTS_FILE | wc -l`
 
-# Solution using r.viewshed
-# =========================
+# Solution using r.los (r.viewshed in GRASS 7.0)
+# ==============================================
 
 echo -ne "\nComputing viewsheds\n"
 
@@ -33,9 +35,9 @@ while read -r line
   PCT_FLOAT=$(echo "100*$((COUNTER+1))/$NPTS" | bc -l)
   PCT=`printf "%0.1f\n" $PCT_FLOAT` 
 
-  echo -ne "Processing $NPTS viewshed instances: \t $PCT % \r" 
+  echo -n "Processing $NPTS viewshed instances: \t $PCT % \r" 
   
-  r.los -c input=dem output=los_${COUNTER} coordinate=$line max_dist=$MAX_VIS_DIST --o --q
+  r.los input=dem output=tmp_los_${COUNTER} coordinate=$line obs_elev=$ELEV max_dist=$MAX_VIS_DIST --o
   COUNTER=$((COUNTER+1))
   
 done < $PTS_FILE
@@ -45,12 +47,15 @@ echo "\n"
 # Combine results in a single map 
 #   (aggregation method doesn't matter as we use
 #   this as a boolean mask)
-r.series in=`g.mlist --q type=rast pat=los_* sep=,` out=total_los method=sum --o
+r.series in=`g.mlist --q type=rast pat=tmp_los_* sep=,` out=total_los method=sum --o --v
 
 # Create distance to road map
-v.to.rast in=road out=road use=val val=1 --o
-r.grow.distance -m input=road distance=dist_from_road --o --q
+v.to.rast in=road out=road use=val val=1 --o --v
+r.grow.distance -m input=road distance=dist_from_road --o --v
 
 # Use distance to road instead of viewing angle in the
 # viewshed result map 
-r.mapcalc "dist_los = if(total_los, dist_from_road, null())" --o --q
+r.mapcalc "dist_los = if(total_los, dist_from_road, null())" --o --v
+
+# Clean up, removing the component visibility rasters
+g.mremove -f "tmp_los_*" --v
