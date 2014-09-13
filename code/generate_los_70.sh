13ebc97f909770c9c4e84c6a3ffda3ee2b3d9627
alpha-beta-soup <richard.m.law@gmail.com>
2014-09-14 10:42:04 +1200

    Added a GRASS 7.0 version of the script, which leverages the improved performance of r.viewshed, rather than GRASS 6.4's r.los, which is very slow. Other changes to certain commands were required for 7.0.

--- code/generate_los_70.sh
+++ code/generate_los_70.sh
@@ -4,20 +4,9 @@ LINE_SHP='../data/roads.shp' # Roads shapefile
 R_DEM='../data/nidemreproj' # Elevation DEM
 PTS_FILE="../data/road_points_coords.csv" # An intermediate output of this script; coordinates of LINE_SHP
 R_RES=25 # The resolution of the DEM (metres)
-DIST_PTS=25 # Ideally the resolution of the DEM
+DIST_PTS=25 # Maximum distance between observer points: ideally the resolution of the DEM
 MAX_VIS_DIST=30000 # Maximum distance visible
 ELEV=1.2 # Metres above the ground that the observer stands (note, try include the vehicle, too)
-
-# r.los takes a long time, and the manual says to keep the number of rows and columns
-#   "under 1000". Here we adjust MAX_VIS_DIST by considering the resolution of
-#   the raster (R_RES), so that there is a maximum of 1000 rows and columns
-POS_VIS_DIST=$(echo "$R_RES * 1000 / 2" | bc -l) # The maximum possible vis dist to use and still have 1000 rows and cols
-if [ 1 -eq `echo "$POS_VIS_DIST < $MAX_VIS_DIST" | bc` ]
-then
-  MAX_VIS_DIST=$POS_VIS_DIST
-else
-  MAX_VIS_DIST=$MAX_VIS_DIST
-fi
 
 # Load shapefile into GRASS
 v.in.ogr dsn=$LINE_SHP output=road --o --v
@@ -26,10 +15,10 @@ v.in.ogr dsn=$LINE_SHP output=road --o --v
 r.in.gdal --o input=$R_DEM output=dem --verbose
 
 # Sample points along line
-v.to.points -ivt in=road out=roads_points dmax=$DIST_PTS --o --q
+v.to.points -it in=road out=roads_points dmax=$DIST_PTS --o --q
 
 # Put point coordinates in text file
-v.out.ascii -r in=roads_points fs=, --quiet | awk -F "\"*,\"*" '{print $1","$2}' > $PTS_FILE
+v.out.ascii in=roads_points separator=, --quiet | awk -F "\"*,\"*" '{print $1","$2}' > $PTS_FILE
 
 NPTS=`cat $PTS_FILE | wc -l`
 
@@ -45,7 +34,7 @@ while read -r line
   PCT_FLOAT=$(echo "100*$((COUNTER+1))/$NPTS" | bc -l)
   PCT=`printf "%0.1f\n" $PCT_FLOAT`
 
-  echo -ne "Processing $NPTS viewshed instances: \t $PCT % ($COUNTER/$NPTS) \r" 
+  echo -ne "Processing $NPTS viewshed instances: \t $PCT % ($((COUNTER+1))/$NPTS) \r" 
   
   # Set the region to a smaller subset around the current observer point
   #   to speed processing
@@ -55,33 +44,37 @@ while read -r line
   E=$(echo "$x+$MAX_VIS_DIST" | bc -l)
   N=$(echo "$y+$MAX_VIS_DIST" | bc -l)
   S=$(echo "$y-$MAX_VIS_DIST" | bc -l)
-  g.region n=$N s=$S e=$E w=$W
+  g.region n=$N s=$S e=$E w=$W --q
   
   # Does not overwrite, so SIGINT (Ctrl+C) can be used to interrupt a
   #  long-running process, to be resumed later
   #  (keep parameters constant between runs)
-  r.los input=dem output=tmp_los_${COUNTER} coordinate=$line obs_elev=$ELEV max_dist=$MAX_VIS_DIST --v
+  r.viewshed -crb input=dem output=tmp_los_${COUNTER} coordinates=$line obs_elev=$ELEV max_dist=$MAX_VIS_DIST memory=2000 --o --q
   COUNTER=$((COUNTER+1))
   
 done < $PTS_FILE
-
-# Set computational region to full extent
-g.region -pm rast=dem --verbose
 
 # Combine results in a single map 
 #   (aggregation method doesn't matter as we use
 #   this as a boolean mask)
 echo "\nCombining component viewsheds\n"
 # Loops because it can exceed hard limit of number of rasters that can be open at once (1024)
+# r.series -z flag, new in grass70, "don't keep files open"
 for i in `seq 0 9`;
 do
-      r.series in=`g.mlist --q type=rast pat=tmp_los_$i* sep=,` out=total_los_$i method=sum --o --q
+  # Set computational region to full extent
+  g.region -pm rast=dem --q
+  # Combine a subset of all the viewsheds
+  # * is a wildcard for zero or more characters
+  r.series -z input=`g.mlist --q type=rast pattern=tmp_los_*$i sep=,` out=total_los_$i method=sum --o --q
 done
 # Then combine the series 0-9 into the final LOS raster
-r.series in `g.mlist --q type=rast pat=total_los_* sep=,` out=total_los method=sum --o --q
+g.region -pm rast=dem --q
+r.series -z input=`g.mlist --q type=rast pattern=total_los_* sep=,` out=total_los method=sum --o --q
 
 # Create distance to road map
 echo "\nDetermining distance from roads\n"
+g.region -pm rast=dem --q
 v.to.rast in=road out=road use=val val=1 --o --q
 r.grow.distance -m input=road distance=dist_from_road --o --q
 
@@ -92,6 +85,6 @@ r.mapcalc "dist_los = if(total_los, dist_from_road, null())"
 
 # Clean up, removing the component visibility rasters
 echo "\nDeleting temporary files\n"
-g.mremove -f "tmp_los_*" --q
+g.mremove -f type=rast pattern="tmp_los_*" --q
 
 echo "\ngenerate_los.sh complete\n"
