4cb4a883913042bb9f9e0b5258e82eea6250b784
alpha-beta-soup <richard.m.law@gmail.com>
2015-05-18 20:53:56 +1200

    Update web-map functionality; now includes a layer control and is generally nicer

--- source/make_map.js
+++ source/make_map.js
@@ -1,18 +1,50 @@
-// initialize the map on the "map" div with a given center and zoom
-var map = L.map('map').setView([-38.6875, 176.0694], 7);
 
-L.tileLayer('http://{s}.tiles.mapbox.com/v3/alpha-beta-soup.jhokb32m/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18
-}).addTo(map);
-                        
-var carViewshed = '/tiles/Mapnik/{z}/{x}/{y}.png'
+var carViewshed = '/data/tiles/Mapnik/{z}/{x}/{y}.png';
+var southWest = [-41.722,171.748];
+var northEast = [-36.441,179.124];
+var bounds = [southWest, northEast];
 
-L.tileLayer(carViewshed, {
-  attribution: 'Richard Law',
-  maxZoom: 15,
+var map = new L.Map('map');
+map.setView([-38.6875, 176.0694], 7); //centre and zoom of map, initially
+map.setMaxBounds(bounds);
+map.options.minZoom = 7;
+map.options.maxZoom = 13;
+
+
+var car = L.tileLayer(carViewshed, {
+  attribution: 'Richard Law | \
+  Pierre Roudier | \
+  <a href="http://maps.scinfo.org.nz/">Landcare Research</a> <a href="http://creativecommons.org/licenses/by/3.0/nz/">CC BY 3.0 NZ</a> | \
+  <a href="https://koordinates.com/layer/40-nz-road-centrelines-topo-150k/">LINZ</a> <a href="http://creativecommons.org/licenses/by/3.0/nz/">CC BY 3.0 NZ</a> | \
+  <a href="http://grass.osgeo.org/">GRASS GIS</a>',
+  bounds: bounds,
+  minZoom: 7,
+  maxZoom: 13,
   tms: true,
-  isBaseLayer: false,
-  opacity: 0.7,
-  transparent: true
-}).addTo(map);
+  isBaseLayer: true,
+  //opacity: 0.7,
+  //transparent: true
+}).addTo(map); //default layer, so add to map
+
+var train = L.tileLayer(carViewshed, {
+  attribution: 'Richard Law | \
+  Pierre Roudier | \
+  Landcare Research and licensed by Landcare Research for re-use under <a href="http://creativecommons.org/licenses/by/3.0/nz/">Creative Commons CC-BY New Zealand license</a> | \
+  <a href="http://grass.osgeo.org/">GRASS GIS</a>',
+  bounds: bounds,
+  minZoom: 7,
+  maxZoom: 13,
+  tms: true,
+  isBaseLayer: true,
+  //opacity: 0.7,
+  //transparent: true
+}); //TODO make train tiles, too //non-default layer, so don't add to map
+
+var viewshedBasemaps = {
+    "Car/bus view": car,
+    "Overlander view": train
+};
+
+L.control.layers(viewshedBasemaps).addTo(map); // Add the layer control to the map
+
+
